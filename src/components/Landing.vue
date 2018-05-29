6b3f06f235b0a1709f77ef7f778fd3828d254de2
eric98 <ergare.17@gmail.com>
2018-05-29 21:56:21 +0200

    Sync

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -13,6 +13,9 @@
         </a></li>
       </ul>
     </nav>
+    <div class="main flex flex-column align-center">
+<h2>AdminLTE Replica</h2>
+    </div>
   </div>
 </template>
 
@@ -25,6 +28,26 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
 
+  .main {
+    height: 729px;
+    background: url(https://i.pinimg.com/originals/be/f4/84/bef484c1c2679790df69058bcd570d53.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    color: white;
+  }
+
+  .flex { display: flex; }
+  .flex-column { flex-direction: column; }
+  .align-center { align-items: center; }
+
+  .header-nav {
+    background-color: #31708F;
+    color: #EEEEEE;
+    width: 100%;
+  }
+
   ul {
     list-style: none;
   }
@@ -35,7 +58,7 @@ export default {
   }
 
   body {
-    background-color: #8abbde;
+    background-color: #d2d6de;
   }
 
   nav {
